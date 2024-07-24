3b743958a7a06317769140a79b4b4fd817da9872
Ruan Fumin <ruanfm@qq.com>
2024-07-24 15:12:33 +0800

    增加备案号

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <t-footer class="footer"> Copyright @ {{ new Date().getFullYear() }} 阮福民 版权所有 </t-footer>
+    <t-footer class="footer"> Copyright @ {{ new Date().getFullYear() }} 阮福民 版权所有 | <t-link href="https://beian.miit.gov.cn/" target="_blank">皖ICP备2024036993号-1</t-link> </t-footer>
 </template>
 
 <style scoped>
