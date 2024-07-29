8451f616bf1f9881f540fc1961cc5f1604d78ab7
Ruan Fumin <ruanfm@qq.com>
2024-07-29 17:18:47 +0800

    更新表格3

--- src/components/tables/table3.vue
+++ src/components/tables/table3.vue
@@ -3,10 +3,8 @@
         <t-card class="c1" title="鸠江区三线水位测站记录情况登记表3" header-bordered hover-shadow>
             <t-list :split="true">
                 <t-list-item>1. 当前整点时间对应水位</t-list-item>
-                <t-list-item>2. 隐藏列: 4小时前整点时间对应水位</t-list-item>
-                <t-list-item>3. 11 点前为昨日 8:00 水位; 11点后为今日8:00水位</t-list-item>
-                <t-list-item>4. 当前整点对比今日或昨日 8:00 两比 涨/落</t-list-item>
-                <t-list-item>5. 四小时内两比 涨/落</t-list-item>
+                <t-list-item>2. 11 点前为昨日 8:00 水位; 11点后为今日8:00水位</t-list-item>
+                <t-list-item>3. 当前整点对比今日或昨日 8:00 两比 涨/落</t-list-item>
             </t-list>
             <template #footer>
                 <t-tooltip :content="message">
