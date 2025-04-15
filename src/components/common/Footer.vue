464a0627a222d7bc1f4f203e3c0b60b44d224266
Ruan Fumin <ruanfm@qq.com>
2025-04-15 20:30:47 +0800

    增加备案信息

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,13 +1,39 @@
 <template>
-    <footer>Copyright © 2024 - 2025 RuanFumin. All Rights Reserved.</footer>
+    <footer>
+        <div class="copyright">Copyright © 2024 - 2025 RuanFumin. All Rights Reserved. <span class="beian"><a
+                    href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">皖ICP备2024036993号-2</a></span>
+        </div>
+        <div class="tishi">建议您使用Chrome、Firefox、Edge、IE10及以上版本和360等主流浏览器浏览本网站</div>
+    </footer>
 </template>
 
 <style scoped>
+div {
+    padding: 0;
+    margin: 0;
+}
+
 footer {
     border-top: 0.5px solid #5c5c5c;
-    height: 60px;
-    line-height: 60px;
+    height: auto;
+    line-height: 32px;
     text-align: center;
     color: #5c5c5c;
+}
+
+.beian {
+    text-align: center;
+    color: #5c5c5c;
+}
+
+.beian a {
+    margin: 0;
+    padding: 0;
+    color: #5c5c5c;
+    text-decoration: none;
+}
+
+.tishi {
+    text-align: center;
 }
 </style>
